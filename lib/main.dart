dd0825a9d682c5e3d151de8d4acccb22f4537c86
avriljeanmarc <avriljeanmarc@yahoo.fr>
2022-09-25 06:04:49 -0400

    Sep. 25, 2022

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,80 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'more.dart';
 import 'model.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() => runApp(const Stockmax());
+void main() {
+  late BuildContext _context;
+  runZonedGuarded(() {
+    runApp(ChangeNotifierProvider<DataCenter>(
+      create: (BuildContext context) => DataCenter(),
+      builder: (context, Stockmax) {
+        _context = context;
+        return MaterialApp(
+          localizationsDelegates: const [
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate ,
+          ],
+          supportedLocales: const [
+            Locale('en'),
+            Locale('fr'),
+          ],
+          title: 'Stockmax',
+          theme: ThemeData(
+            brightness: Brightness.light,
+            primaryColor: Colors.lightBlue[800],
+          ),
+          home: const Log(),
+        );
+      },
+    ));
+  }, (er, st) {
+    print('${er.toString()}\n${st.toString()}');
+    _context.read<DataCenter>().flutterError =
+        '${er.toString()}\n${st.toString()}';
+  });
 
-class Stockmax extends StatelessWidget {
+  /*runZoned(
+    () {
+      runApp(const Stockmax());
+    },
+    onError: (error) {
+      print('My errors caught: $error.toString()');
+    },
+    zoneSpecification: new ZoneSpecification(
+      print: (Zone self, ZoneDelegate parent, Zone zone, String line) {
+        parent.print(zone, 'Intercepted flutter error :$line');
+      },
+    ),
+  );*/
+  //runApp(const Stockmax());
+}
+
+/*class Stockmax extends StatelessWidget {
   const Stockmax({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<DataCenter>(
-      create: (BuildContext context) => DataCenter(),
-      child: MaterialApp(
-        localizationsDelegates: const [
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-        ],
-        supportedLocales: const [
-          //Locale('zh', 'CH'),
-          Locale('en', 'US'),
-          Locale('fr', 'FR'),
-        ],
-        title: 'Stockmax',
-        theme: ThemeData(
-          brightness: Brightness.light,
-          primaryColor: Colors.lightBlue[800],
-          /*textTheme: const TextTheme(
-            /*headline6: TextStyle(
-              fontSize: 25.0,
-            ),*/
-            bodyText2: TextStyle(
-              fontSize: 20.0,
-            ),
-            subtitle1: TextStyle(
-              fontSize: 18.0,
-            ),
-          ),*/
-        ),
-        home: const Log(),
+    return MaterialApp(
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'),
+        Locale('fr'),
+      ],
+      title: 'Stockmax',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.lightBlue[800],
       ),
+      home: const Log(),
     );
   }
-}
+}*/
 
 /*
 class MyLog extends StatefulWidget {
@@ -92,85 +122,5 @@ class _MyLogState extends State<MyLog> {
   }
 }
 
-
-class Stockmax extends StatelessWidget {
-  const Stockmax({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider<CounterViewModel>(
-      create: (BuildContext context) => CounterViewModel(),
-      child: MaterialApp(
-        //debugShowCheckedModeBanner: false,
-        title: 'Stockmax',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ), //ThemeData.dark(),
-        home: const MyLog(), //Log(),
-      ),
-    );
-  }
-}
 */
 
-/*
-class MyLog extends StatelessWidget {
-  const MyLog({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'MyTitle',
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Counter: ${Provider.of<CounterViewModel>(context).counter}',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Text(
-              'AnotherCounter: ${Provider.of<CounterViewModel>(context).anotherCounter}',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            ElevatedButton(
-              child: const Text(
-                'Increment anotherCounter',
-              ),
-              onPressed: Provider.of<CounterViewModel>(context, listen: false)
-                  .incrementAnotherCounter,
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: Provider.of<CounterViewModel>(context, listen: false)
-            .incrementCounter,
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-class CounterViewModel extends ChangeNotifier {
-  int _counter = 0;
-  int _anotherCounter = 0;
-
-  int get counter => _counter;
-  int get anotherCounter => _anotherCounter;
-
-  void incrementCounter() {
-    _counter++;
-    notifyListeners();
-  }
-
-  void incrementAnotherCounter() {
-    _anotherCounter++;
-  }
-}
-*/
